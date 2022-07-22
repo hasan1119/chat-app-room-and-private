65aab5c4f792c248cdbcd677e1beb636f19a4528
hasan1119 <hasan63251119@gmail.com>
2022-07-22 07:21:52 +0600

    kdfjkd

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const socket = require('socket.io')
+const app = express();
+
+const http = require("http");
+const expressHTTPServer = http.createServer(app);
+const io = new socket.Server(expressHTTPServer)
+
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`)
+})
+
+
+io.on('connection', (socket) => {
+
+    io.to(socket.id).emit('getName')
+    
+
+    socket.on("new-message", (msg, cb) => {
+       socket.broadcast.emit("receive_msg", msg, socket.name)
+       cb()
+    })
+    socket.on("setName", (name, cb) => {
+        socket.name = name;
+        cb()
+    })
+})
+
+
+
+expressHTTPServer.listen(3000, () => {
+    console.log("Server is running on port @3000");
+})
+
