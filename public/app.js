cfd59fdd86f0ced35bd902256da37a3c5f72a0df
hasan1119 <hasan63251119@gmail.com>
2022-07-24 13:48:25 +0600

    Hello

--- public/app.js
+++ public/app.js
@@ -4,18 +4,23 @@ const socket = io();
 // selections
 const nameForm = document.getElementById('name_form');
 const msgForm = document.getElementById('msg_form');
+const publicRoomsDiv = document.getElementById('accordionPanelsStayOpenExample');
+const roomCreateBtn = document.getElementById('create-btn');
+const roomNameInputEl = document.getElementById('create_room');
 const onlineUserList = document.getElementById('onlineUserList');
 const nameFormArea = document.querySelector(".name");
 const messages = document.querySelector(".messages");
 const roomArea = document.querySelector('.room')
 const innerCanvas = document.querySelector('.inner_canvas')
 const displayName = document.querySelector('.displayNmae')
+const modal = document.querySelector(".modal");
 innerCanvas.hidden = true;
 
 
 
 // global variables
 let activeUsers;
+let publicRooms;
 
 
 
@@ -40,6 +45,7 @@ socket.on("get_active_users", (users) => {
         li.style.cursor= 'pointer'
         li.addEventListener('click', () => {
             openCanvas(user)
+            msgForm[1].dataset.room = false;
             messages.innerHTML = ''
 
         })
@@ -59,8 +65,10 @@ msgForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const msg = msgForm[0].value;
     const id = msgForm[1].value;
+    const isRoom = msgForm[1].dataset.room;
+
     if (msg) {
-        socket.emit("send_a_msg", {msg, id}, () => {
+        socket.emit("send_a_msg", {msg, id, isRoom}, () => {
             const li = document.createElement("li");
             li.classList.add("list-group-item");
             li.textContent ="You" + ": " + msg;
@@ -76,11 +84,23 @@ msgForm.addEventListener('submit', (e) => {
 
 socket.on("receive_a_message", (data, senderId) => {
 
+    const isRoom = data.isRoom;
    
     const user = activeUsers.find(u => u.id === data.id);
-    openCanvas(user)    
+     
+
     const sender = activeUsers.find(u => u.id ===senderId);
-    openCanvas(sender) 
+    if (isRoom) {
+
+        innerCanvas.hidden = false;
+        displayName.textContent = data.id;
+        msgForm[1].value = data.id;
+        msgForm[1].dataset.room = true;
+    } else {
+        openCanvas(sender) 
+        msgForm[1].dataset.room = false;
+    }
+
     const li = document.createElement("li");
     li.classList.add("list-group-item");
     li.textContent =sender.name + ": " + data.msg;
@@ -93,9 +113,94 @@ socket.on("receive_a_message", (data, senderId) => {
 
 
 // open the msg canvas
-
 function openCanvas(user) {
     innerCanvas.hidden = false;
     displayName.textContent = user.name;
     msgForm[1].value = user.id;
+}
+
+
+// create room functionality
+roomCreateBtn.addEventListener('click', (e) => {
+    const roomName = roomNameInputEl.value;
+    if (roomName) {
+        socket.emit("create_room", roomName, () => {
+            modalClose()
+        })
+    }
+})
+
+
+// get public rooms
+socket.on('getPublicRooms', (rooms) => {
+    publicRooms = rooms;
+    publicRoomsDiv.innerHTML = ''
+    rooms.forEach(room => {
+        const accordionItem = document.createElement('div');
+        accordionItem.classList.add("accordion-item")
+        
+        accordionItem.innerHTML = `
+        <h2 class="accordion-header" id="${room.id}id">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#${room.id}option"
+                  aria-expanded="false"
+                  aria-controls="${room.id}option"
+                  
+                >
+                  ${room.roomName} (${room.size})
+    
+                  <span onclick="joinRoom('${room.roomName}')"  class="material-symbols-outlined">
+                      group_add
+                  </span>
+                </button>
+              </h2>
+              <div
+                id="${room.id}option"
+                class="accordion-collapse collapse"
+                aria-labelledby="${room.id}id"
+              >
+                <div class="accordion-body">
+                  <ul id="participants"></ul>
+                </div>
+              </div>`;
+
+
+        const participantUl = accordionItem.querySelector('#participants');
+
+        room?.participants?.forEach(participant => {
+            const li = document.createElement('li');
+            li.textContent = participant.name;
+            participantUl.appendChild(li)
+        })
+
+        publicRoomsDiv.appendChild(accordionItem);
+
+   })
+})
+
+
+
+function joinRoom(roomName) {
+    socket.emit("joinRoom", roomName, () => {
+        messages.innerHTML = ''
+        innerCanvas.hidden = false;
+        displayName.textContent = roomName;
+        msgForm[1].value = roomName;
+        msgForm[1].dataset.room = true;
+     })
+}
+
+
+
+// modal close 
+function modalClose() {
+    modal.classList.remove("show");
+    modal.style.display = "none";
+    roomNameInputEl.value = "";
+    document.body.classList.remove("modal-open");
+    document.body.style = {};
+    document.querySelector(".modal-backdrop")?.remove("show");
 }
--- room.js
+++ room.js
@@ -30,11 +30,53 @@ io.on('connection', (socket) => {
                     name: userSocket.name,
                 })
             }
-            
+
         })
 
         return activeUser;
     }
+
+    // get rooms 
+    const getPublicRooms = async () => {
+        const rooms = await io.sockets.adapter.rooms;
+        const sids = await io.sockets.adapter.sids;
+        const allSockets = await io.sockets.sockets;
+
+        const roomkeys = [...rooms.keys()];
+        const sidKeys = [...sids.keys()];
+
+        const publicRooms = [];
+        let roomId = 0;
+        for (let roomName of roomkeys) {
+            if (!sidKeys.includes(roomName)) {
+                const participantSet = rooms.get(roomName);
+                const size = participantSet.size;
+
+                const participants = []
+
+                for (let id of [...participantSet]) {
+                    const userSocket = allSockets.get(id);
+                    participants.push({
+                        id: userSocket.id,
+                        name : userSocket.name
+                    })
+                }
+
+
+                publicRooms.push({
+                    id: "a" + roomId + Date.now(),
+                    roomName,
+                    size,
+                    participants
+                })
+
+                ++roomId;
+            }
+        }
+
+        return publicRooms;
+    }
+
 
 
     // set name event
@@ -42,27 +84,59 @@ io.on('connection', (socket) => {
         socket.name = name;
         cb()
         const activeUsers = await getOnlineUsers();
-       io.emit("get_active_users", activeUsers);
+        io.emit("get_active_users", activeUsers);
+
+        const publicRooms = await getPublicRooms();
+        io.emit('getPublicRooms', publicRooms)
 
     })
+
+
 
 
     // disconnect event
     socket.on('disconnect', async () => {
+
         const activeUsers = await getOnlineUsers();
-       io.emit("get_active_users", activeUsers);
+        io.emit("get_active_users", activeUsers);
+
+        const publicRooms = await getPublicRooms();
+        io.emit('getPublicRooms', publicRooms)
     })
 
-
+    // send a private message
     socket.on("send_a_msg", (data, cb) => {
         const id = data.id;
         const msg = data.msg;
+        const isRoom = data.isRoom === 'false' ? false : data.isRoom;
 
-        io.to(id).emit("receive_a_message",data, socket.id )
-        cb()
+
+        if (isRoom) {
+            socket.to(id).emit("receive_a_message", data, socket.id)
+            cb()
+        } else {
+            io.to(id).emit("receive_a_message", data, socket.id)
+            cb()
+        }
 
     })
 
+    // create a public room
+    socket.on("create_room", async (roomName, cb) => {
+        socket.join(roomName);
+        const publicRooms = await getPublicRooms();
+        io.emit('getPublicRooms', publicRooms)
+        cb()
+    })
+
+
+    // join room
+    socket.on("joinRoom", async (roomName, cb) => {
+        socket.join(roomName);
+        const publicRooms = await getPublicRooms();
+        io.emit('getPublicRooms', publicRooms)
+        cb()
+    })
 
 })
 
